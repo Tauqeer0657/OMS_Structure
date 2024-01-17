1a92b58387e7b95e4b882afdf95fad0a5e683f83
nawaid <iqbalnawaid15@gmail.com>
2024-01-17 13:06:36 +0530

    Structure

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+// const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = process.env.PORT || 3306;
+
+app.use(cookieParser());
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
